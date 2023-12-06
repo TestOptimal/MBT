266661cc9862f03cee2851d109c1cd08d93b6d0a
Yaxiong Lin <lin.yaxiong@gmail.com>
2023-12-06 16:15:28 -0600

    push to dockerhub

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,3 +1,7 @@
 # docker build --progress=plain --no-cache -t mbt .
 docker build --progress=plain -t mbt .
 
+# docker login -u linyaxiong
+# docker image tag mbt linyaxiong/mbt:latest
+# docker image tag mbt linyaxiong/mbt:latest
+# docker run -p 8888:8888 linyaxiong/mbt:latest
