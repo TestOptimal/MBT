fbad4a4f5324958db795788ee4b8c9bd21b77ee4
Yaxiong Lin <lin.yaxiong@gmail.com>
2023-12-06 17:33:43 -0600

    fix docker to use testoptimal name

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,7 +1,7 @@
 # docker build --progress=plain --no-cache -t mbt .
 docker build --progress=plain -t mbt .
 
-# docker login -u linyaxiong
-# docker image tag mbt linyaxiong/mbt:latest
-# docker image tag mbt linyaxiong/mbt:latest
-# docker run -p 8888:8888 linyaxiong/mbt:latest
+# docker login -u testoptimal
+# docker image tag mbt testoptimal/mbt:latest
+# docker image push testoptimal/mbt:latest
+# docker run -p 8888:8888 testoptimal/mbt:latest
