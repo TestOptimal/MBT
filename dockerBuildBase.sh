43049479b7fbd2dcde43c2f62178d378ae200740
Yaxiong Lin <lin.yaxiong@gmail.com>
2023-12-14 21:09:05 -0600

    updated docker build

--- dockerBuildBase.sh
+++
@@ -1,8 +0,0 @@
-# docker build --progress=plain --no-cache -t testoptimal/base -f DockerfileBase .
-docker build --progress=plain -t testoptimal/base -f DockerfileBase .
-
-# docker login -u testoptimal
-# docker image tag testoptimal/mbt base:latest
-# docker image push testoptimal/base:latest
-# docker run -p 8888:8888 testoptimal/base:latest
-# docker system prune
